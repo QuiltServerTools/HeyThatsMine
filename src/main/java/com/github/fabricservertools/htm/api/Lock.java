4c82877634c3ec8fcd58b34732cf6873a6ee5458
Eclipse <116838833+eclipseisoffline@users.noreply.github.com>
2024-05-27 20:04:46 +0000

    Update to 1.20.5 (#44)
    Co-authored-by: Will <potatoboy9999@outlook.com>

--- src/main/java/com/github/fabricservertools/htm/api/Lock.java
+++ src/main/java/com/github/fabricservertools/htm/api/Lock.java
@@ -2,6 +2,7 @@ package com.github.fabricservertools.htm.api;
 
 import com.github.fabricservertools.htm.HTMContainerLock;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public interface Lock {
@@ -11,9 +12,9 @@ public interface Lock {
 
 	void onInfo(ServerPlayerEntity player, HTMContainerLock lock);
 
-	NbtCompound toTag();
+	NbtCompound toTag(RegistryWrapper.WrapperLookup registryLookup);
 
-	void fromTag(NbtCompound tag);
+	void fromTag(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup);
 
 	LockType<?> getType();
 }
