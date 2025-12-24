2aa250b45152ffff4fccf0ce14c0ce97bede07b4
Eclipse <eclipse@eclipseisoffline.xyz>
2025-12-24 13:50:14 +0000

    Fix: don't register HTM subcommands when using /reload (#68)

--- src/main/java/com/github/fabricservertools/htm/command/HTMCommand.java
+++ src/main/java/com/github/fabricservertools/htm/command/HTMCommand.java
@@ -1,5 +1,14 @@
 package com.github.fabricservertools.htm.command;
 
+import com.github.fabricservertools.htm.command.subcommands.FlagCommand;
+import com.github.fabricservertools.htm.command.subcommands.InfoCommand;
+import com.github.fabricservertools.htm.command.subcommands.PersistCommand;
+import com.github.fabricservertools.htm.command.subcommands.QuietCommand;
+import com.github.fabricservertools.htm.command.subcommands.RemoveCommand;
+import com.github.fabricservertools.htm.command.subcommands.SetCommand;
+import com.github.fabricservertools.htm.command.subcommands.TransferCommand;
+import com.github.fabricservertools.htm.command.subcommands.TrustCommand;
+import com.github.fabricservertools.htm.command.subcommands.UntrustCommand;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import me.lucko.fabric.api.permissions.v0.Permissions;
@@ -29,4 +38,16 @@ public class HTMCommand {
         }
         SUB_COMMANDS.add(subCommand);
 	}
+    
+    public static void bootstrap() {
+        registerSubCommand(new SetCommand());
+        registerSubCommand(new RemoveCommand());
+        registerSubCommand(new TrustCommand());
+        registerSubCommand(new UntrustCommand());
+        registerSubCommand(new InfoCommand());
+        registerSubCommand(new TransferCommand());
+        registerSubCommand(new FlagCommand());
+        registerSubCommand(new PersistCommand());
+        registerSubCommand(new QuietCommand());
+    }
 }
